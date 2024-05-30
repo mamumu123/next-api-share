9ff487d977dd6adb4a3a818a2b9167004609d413
lem <11597313+lemC@user.noreply.gitee.com>
2024-05-30 23:17:10 +0800

    chore: canvas

--- src/app/(root)/segment/page.tsx
+++ src/app/(root)/segment/page.tsx
@@ -1,20 +1,36 @@
 "use client";
 
-import React from "react";
+import React, { useRef } from "react";
 import { pipeline, env } from '@xenova/transformers';
 import { Button } from "@/components/ui/button";
 env.allowLocalModels = false;
 
 export default function Page() {
+    const canvasRef = useRef<HTMLCanvasElement>(null)
     const onStart = async () => {
         const segmenter = await pipeline('image-segmentation', 'Xenova/face-parsing');
         console.log('start');
-        const output = await segmenter('/canvas.png');
+        const output = await segmenter('/a.png');
         console.log('output end', output);
+        const map = {}
+        Object.keys(output).forEach((key: any) => {
+            const item = output[key];
+            const { } = item;
+            // map[item]
+        });
+        // for (const m of output) {
+        //     m.mask.save(`${m.label}.png`);
+        // }
+        const ctx = canvasRef.current?.getContext('2d');
+        if (!ctx) {
+            return
+        }
+        // ctx.s
     }
     return (
         <div className='mt-5'>
             <Button onClick={onStart}>开始</Button>
+            <canvas width={512} height={512} ref={canvasRef}></canvas>
         </div>
     );
 }
